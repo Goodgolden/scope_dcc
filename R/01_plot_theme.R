784a8b5aa4e2c7def19917608b9b5a4e3920a640
Randy <51439318+Goodgolden@users.noreply.github.com>
2025-04-04 13:14:27 -0600

    add R

---
+++ R/01_plot_theme.R
@@ -0,0 +1,20 @@
+## report font and graphic setup -----------------------------------------------
+
+
+update_geom_defaults(geom = "text",
+                     aes(family = "Open Sans"))
+
+
+#' Title 
+#'
+#' @param ... 
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+
+theme_alt <- function(...) {
+  theme_bw(base_family = "Open Sans") +
+    theme()
+}
