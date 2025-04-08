632f52a43cb11f426d495cb7c31045ae1a8f86c3
Randy <51439318+Goodgolden@users.noreply.github.com>
2025-04-08 11:07:26 -0600

    new_aim function

--- R/00_utils.R
+++ R/00_utils.R
@@ -62,9 +62,12 @@ read_excel_allsheets <- function(filename, tibble = FALSE) {
     devtools.install.args = "",
     ## Arguments to be passed to install.packages
     devtools.name = "Randy Jin",
-    devtools.desc.author = c("Randy Jin <xin.2.jin@cuanschutz.edu> [aut, cre]",
-                             "Elizabeth Juarez-Colunga <elizabeth.juarez-colunga@cuanschutz.edu> [ctb]",
-                             "Katie Colborn <katie.colborn@cuanschutz.edu> [ctb]"),
+    devtools.desc.author = c("Randy Jin 
+                             <xin.2.jin@cuanschutz.edu> [aut, cre]",
+                             "Elizabeth Juarez-Colunga 
+                             <elizabeth.juarez-colunga@cuanschutz.edu> [ctb]",
+                             "Katie Colborn 
+                             <katie.colborn@cuanschutz.edu> [ctb]"),
     ## Package author and maintainer
     devtools.desc.license = "MIT",
     ## Default license for the package
@@ -82,3 +85,98 @@ read_excel_allsheets <- function(filename, tibble = FALSE) {
   ## Ensure the function runs silently
   invisible()
 }
+
+
+# Create QMD template ----------------------------------------------------------
+#' Title Create QMD template
+#'
+#' @param filename \code{character} The name of the file to create.
+#'
+#' @returns \code{character} A string containing the template for the QMD file.
+#' @export
+template <- function(filename) {
+  title <- gsub("_", " ", 
+                tools::file_path_sans_ext(basename(filename)))
+  title <- tools::toTitleCase(title)
+  paste0("---\n",
+         "title: \"", title, "\"\n",
+         "editor: source\n",
+         "---\n\n",
+         "```{r setup, include=FALSE}\n",
+         "knitr::opts_chunk$set(echo = TRUE,\n",
+         "                      warnings = FALSE,\n",
+         "                      message = FALSE,\n",
+         "                      comment = \"#>\",\n",
+         "                      #results = \"hide\",\n",
+         "                      digits = 4,\n",
+         "                      error = FALSE)\n",
+         "```\n\n",
+         "```{css, echo=FALSE}\n",
+         ".scroll-100 {\n",
+         "  max-height: 300px;\n",
+         "  max-width: 1000px;\n",
+         "  overflow-y: auto;\n",
+         "  background-color: inherit;\n",
+         "}\n",
+         "```\n\n",
+         "## Section\n\n",
+         "<!-- Content for ", title, " goes here.-->")
+}
+
+## new_aim---------------------------------------------------------------------
+#' Title Create a new aim folder with all the QMD files
+#'
+#' @param folder \code{character} The name of the folder to create.
+#' @param qmd_files \code{character} A vector of QMD file names to create. Default is a list of common QMD files.
+#' @param overwrite \code{logical} If \code{TRUE}, overwrite existing files. Default is \code{FALSE}.
+#'
+#' @returns \code{NULL} This function does not return a value. 
+#' It creates a folder and populates it with QMD files.
+#' @export
+#'
+#' @examples 
+new_aim <- function(folder,
+                    qmd_files = c("index.qmd", 
+                                  "references.bib",
+                                  "reference.qmd",
+                                  "01_hypothesis.qmd",
+                                  "02_outcomes.qmd", 
+                                  "03_overviews.qmd",
+                                  "04_inclusion.qmd", 
+                                  "05_randomization.qmd",
+                                  "06_sample_size.qmd",
+                                  "07_population.qmd", 
+                                  "08_missing.qmd",
+                                  "09_monitoring.qmd",
+                                  "10_stopping.qmd", 
+                                  "11_software.qmd", 
+                                  "12_figures.qmd",
+                                  "13_tables.qmd", 
+                                  "14_listing.qmd",
+                                  "15_consort.qmd",
+                                  "16_adherence.qmd",
+                                  "17_characteristics.qmd", 
+                                  "18_safety.qmd"),
+                    overwrite = FALSE) {
+  if (missing(folder) || !nzchar(folder)) {
+    stop("You must specify a folder name.")
+  }
+  
+  dir.create(folder,
+             showWarnings = FALSE, 
+             recursive = TRUE)
+  
+  for (file in qmd_files) {
+    full_path <- file.path(folder, file)
+    
+    if (file.exists(full_path) && !overwrite) {
+      message("Skipping existing file: ", full_path)
+      next
+    }
+    
+    writeLines(template(file), con = full_path)
+    message("Created: ", full_path)
+  }
+}
+new_aim(folder = "new_aim",
+        overwrite = TRUE)
